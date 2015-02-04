75c1a5a00904c0dac9c995f274acdd801b0003f5
Matheus Cunha <mathcunha@gmail.com>
2015-02-04 14:22:40 +0000

    collecting resource usage from Mesos Master

--- mmon/mmon.go
+++ mmon/mmon.go
@@ -11,12 +11,14 @@ var Config config
 type config struct {
 	Mesos    string
 	Interval string
+	ES       string
 }
 
 type MesosState struct {
-	activated  int        `json:"activated_slaves"`
-	frameworks []Resource `json:"frameworks"`
-	slaves     []Slave    `json:"slaves"`
+	Activated  int          `json:"activated_slaves"`
+	Frameworks []Frameworks `json:"frameworks"`
+	Slaves     []Slave      `json:"slaves"`
+	Cluster
 }
 
 type Cluster struct {
@@ -24,16 +26,22 @@ type Cluster struct {
 	Used Resource
 }
 
+type Frameworks struct {
+	Active   bool
+	Resource `json:"resources"`
+	Used     Resource `json:"used_resources"`
+}
+
 type Slave struct {
-	Resource
-	Id  string
-	Pid string
+	Resource `json:"resources"`
+	Id       string
+	Pid      string
 }
 
 type Resource struct {
-	cpus int
-	mem  int
-	disk int
+	Cpus float32 `json:"cpus"`
+	Mem  float32 `json:"mem"`
+	Disk float32 `json:"disk"`
 }
 
 func (m *MesosState) Run() {
@@ -46,13 +54,37 @@ func (m *MesosState) Run() {
 
 	dec := json.NewDecoder(resp.Body)
 	if err := dec.Decode(m); err == nil {
+		m.updateCluster()
 		log.Printf("%v", *m)
+		go postES(m.Cluster)
 		return
 	} else {
 		log.Printf("error reading response body (%v) - %v \n", resp, err)
 	}
 }
 
+func (m *MesosState) updateCluster() {
+	m.Cluster.Resource = Resource{0.0, 0.0, 0.0}
+	m.Cluster.Used = Resource{0.0, 0.0, 0.0}
+
+	for _, v := range m.Frameworks {
+		if v.Active {
+			m.Cluster.Used.Cpus += v.Resource.Cpus
+			m.Cluster.Used.Mem += v.Resource.Mem
+			m.Cluster.Used.Disk += v.Resource.Disk
+		}
+	}
+
+	for _, v := range m.Slaves {
+		m.Cluster.Resource.Cpus += v.Resource.Cpus
+		m.Cluster.Resource.Mem += v.Resource.Mem
+		m.Cluster.Resource.Disk += v.Resource.Disk
+	}
+}
+
 func (m *MesosState) Interval() string {
 	return Config.Interval
+}
+
+func postES(c Cluster) {
 }
